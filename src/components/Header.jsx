b7ae996e005bf7a04ad2ca5b51b7015c79d418e6
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2025-12-18 17:46:39 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FiMoon, FiSun } from "react-icons/fi";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <span>MovieSphere</span>
+        </Link>
+
+        <button> 
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
