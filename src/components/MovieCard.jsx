62eaef095b6c49aeebc026778cbe564965f5a1f7
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2025-12-19 12:25:24 +0530

    Movie List done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,31 @@
-const MovieCard = () => {
-    return(
-        <div>
-            <div>
-                <img src="" alt="" />
-                
-            
-            </div>
-            <div>
-                <p></p> {/*title*/}
-                <p></p> {/*year released | language*/}
-                <span></span> {/*ratings*/}
-            </div>
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="w-full max-w-[260px] bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-xl hover:scale-105 transition-all duration-300">
+      
+      <div className="relative">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="w-full h-[360px] object-cover"
+        />
+      </div>
+
+      <div className="p-4">
+        <p className="text-gray-900 text-3xl leading-tight tracking-wide mb-2" style={{ fontFamily: "'Bebas Neue', cursive" }}>
+          {movie.title}
+        </p>
+
+        <div className="flex items-center justify-between text-gray-500 text-sm">
+          <span>
+            {movie.release_date?.slice(0, 4)} · {movie.original_language?.toUpperCase()}
+          </span>
+          <span className="bg-purple-300 text-gray-900 font-semibold px-2 py-1 rounded-lg">
+            {movie.vote_average.toFixed(1)}
+          </span>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,15 +1,32 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const response = fetch("https://api.themoviedb.org/3/movie/popular?api_key=cad28c03141d9237941304b4f19587fa")
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/movie/popular?api_key=cad28c03141d9237941304b4f19587fa"
+        );
 
-        console.log(response)
-    }, [])
-    return(
-        <h1>Movie List</h1>
-    )
-}
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error("Error fetching movies", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
 
 export default MovieList;
